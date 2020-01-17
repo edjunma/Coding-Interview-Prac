f865eb9be5684de06081f3d357932e3fafb0db7a
edjunma <edjunma@gmail.com>
2020-01-17 03:56:15 -0500

    finish binary search question

--- Binary-Search/binary-search.py
+++ Binary-Search/binary-search.py
@@ -1,5 +1,18 @@
 # Python Solution One
+def binarySearch(array, target):
+  return binarySearchHelper(array, target, 0, len(array) - 1)
 
+def binarySearchHelper(array, target, left, right):
+  if left > right:
+    return - 1
+  middle = (left + right) // 2
+  potentialMatch = array[middle]
+  if target == potentialMatch:
+    return middle:
+  elif target < potentialMatch:
+    return binarySearchHelper(array, target, left, middle - 1)
+  else:
+    return binarySearchHelper(array, target, right, middle + 1)
 
 
 # Python Second Solution
