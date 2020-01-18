01e2ece576d026314de39c6f879e22dbce45c62b
edjunma <edjunma@gmail.com>
2020-01-17 23:13:21 -0500

    latest update, doing python solutions to palindrome-check

---
+++ Palindrome-Check/palindrome-check.py
@@ -0,0 +1,9 @@
+# My solution in Python
+# O(n^2) time | O(n) space
+
+
+def isPalindrome(string):
+    reversedString = ""
+    for i in reversed(range(len(string))):
+        reversedString += string[i]
+    return string == reversedString
