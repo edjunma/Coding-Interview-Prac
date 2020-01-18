18bde855f94f392d106abf880c163a00d20d8bf3
edjunma <edjunma@gmail.com>
2020-01-17 22:55:52 -0500

    add insertion sort question in js

---
+++ Insertion-Sort/insertion-sort.js
@@ -0,0 +1,20 @@
+// JavaScript Solution
+// Best: O(n) time | O(1) space
+// Average: O(n^2) time | O(1) space
+// Worst: O(n^2) time | O(1) space
+
+function insertionSort(array) {
+	for (let i = 1; i < array.length; i++) {
+		let j = i;
+		while (j > 0 && array[j] < array[j - 1]) {
+			swap(j, j - 1, array);
+		}
+	}
+	return array;
+}
+
+function swap(i, j, array) {
+	const temp = array[j];
+	array[j] = array[i];
+	array[i] = temp;
+}
