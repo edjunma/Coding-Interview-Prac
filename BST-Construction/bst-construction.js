854d185bd5a10479436c492f5556e7942f10d2e7
Edmond <edjunma@gmail.com>
2020-01-24 02:00:59 -0500

    finish 1st solution

--- BST-Construction/bst-construction.js
+++ BST-Construction/bst-construction.js
@@ -20,5 +20,12 @@ insert (value) {
         } else {
             this.left.insert(value);
         }
+    } else {
+        if (this.right === null) {
+            this.right = new BST(value);
+        } else {
+            this.right.insert(value);
+        }
     }
+    return this;
 }
