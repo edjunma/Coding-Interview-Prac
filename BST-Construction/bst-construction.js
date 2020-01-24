f0e137ef9377fb020b130f46c57b2a8c21a062f4
Edmond <edjunma@gmail.com>
2020-01-24 01:59:17 -0500

    binary-search-tree optimal solutions started with js on bst-construction question

---
+++ BST-Construction/bst-construction.js
@@ -0,0 +1,24 @@
+// JavaScript Solution
+// Binary Search Tree Construction
+// BST class
+
+class BST {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+// Average: O(log(n)) time | O(log(n)) space
+// Worst: O(n) time | O(n) space
+
+insert (value) {
+    if (value < this.value) {
+        if (this.left === null) {
+            this.left = new BST(value);
+        } else {
+            this.left.insert(value);
+        }
+    }
+}
