7822d035f88f340a0fea7f636abdcc44410f73b2
Edmond <edjunma@gmail.com>
2020-01-25 01:29:23 -0500

    working on 2nd python solution to bst-construction

--- BST-Construction/bst-construction.py
+++ BST-Construction/bst-construction.py
@@ -74,3 +74,30 @@ def getMinValue(self):
         return self.value
     else:
         return self.left.getMinValue()
+
+# Solution 2 in Python
+
+class BST: 
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    # Average: O(log(n)) time | O(1) space
+    # Worst: O(n) time | O(1) space
+    def insert(self, value):
+        currentNode = self
+        while True:
+            if value < currentNode.value:
+                if currentNode.left is None:
+                    currentNode.left = BST(value)
+                    break
+                else:
+                    currentNode = currentNode.left
+            else:
+                if currentNode.right is None:
+                    currentNode.right = BST(value)
+                    break
+                else:
+                    currentNode = currentNode.right
+        return self
