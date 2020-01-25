33aa62ca23f908cbf89fd9bef459806026da1b4b
Edmond <edjunma@gmail.com>
2020-01-25 01:09:55 -0500

    working on 2nd solution to bst construction

--- BST-Construction/bst-construction.js
+++ BST-Construction/bst-construction.js
@@ -89,5 +89,71 @@ remove(value, parent = null) {
 }
 
 getMinValue() {
-    
+    if (this.left === null) {
+        return this.value;
+    } else {
+        return this.left.getMinValue();
+    }
+}
+
+// 2nd solution
+
+class BST {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+// Average: O(log(n)) time | O(1) space
+// Worst: O(n) time | O(1) space
+insert(value) {
+    let currentNode = this;
+    while (true) {
+        if (value < currentNode.value) {
+            if (currentNode.left === null) {
+                currentNode.left = new BST(value);
+                break;
+            } else {
+                currentNode = currentNode.left;
+            }
+        } else {
+            if (currentNode.right === null) {
+                currentNode.right = new BST(value);
+                break;
+            } else {
+                currentNode = currentNode.right;
+            }
+        }
+    }
+    return this;
+}
+
+// Average: O(log(n)) time | O(1) space
+// Worst: O(n) time | O(1) space
+
+contains(value) {
+    let currentNode = this;
+    while (currentNode !== null) {
+        if (value < currentNode.value) {
+            currentNode = currentNode.left;
+        } else if (value > currentNode.value) {
+            currentNode = currentNode.right;
+        } else {
+            return true;
+        }
+    }
+    return false;
+}
+
+// Average: O(log(n)) time | O(1) space
+// Worst: O(n) time | O(1) space
+
+remove(value, parentNode = null) {
+    let currentNode = this;
+    while (currentNode !== null) {
+        parentNode = currentNode;
+        currentNode = currentNode.left;
+    }
 }
