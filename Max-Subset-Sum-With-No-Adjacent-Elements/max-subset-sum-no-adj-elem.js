2e98858b47bbd94954140c6bd5cc4c662e3cc972
edjunma <edjunma@gmail.com>
2020-01-28 02:56:44 -0500

    starting on max subset sum with no adj elements

---
+++ Max-Subset-Sum-With-No-Adjacent-Elements/max-subset-sum-no-adj-elem.js
@@ -0,0 +1,8 @@
+// JavaScript Solution
+// Solution #1
+// O(n) time | O(n) space
+
+function maxSubsetSumNoAdjacent(array) {
+	if (!array.length) return 0;
+	if (array.length === 1) return array[0];
+}
