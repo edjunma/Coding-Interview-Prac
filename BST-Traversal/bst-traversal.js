d1b0b1224a8218c5f05f3a940f59e30079db91cf
edjunma <edjunma@gmail.com>
2020-01-28 02:43:09 -0500

    starting on bst-traversal

---
+++ BST-Traversal/bst-traversal.js
@@ -0,0 +1,11 @@
+// JavaScript Solution
+// O(n) time | O(n) space
+
+function inOrderTraverse(tree, array) {
+	if (tree !== null) {
+		inOrderTraverse(tree.left, array);
+		array.push(tree.value);
+		inOrderTraverse(tree.right, array);
+	}
+	return array;
+}
