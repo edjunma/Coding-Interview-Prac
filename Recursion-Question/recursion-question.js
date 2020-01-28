6312adbf16592594dc19bfab4d1eb158ae00f9bf
edjunma <edjunma@gmail.com>
2020-01-28 15:17:36 -0500

    add recursion question, only in javascript

---
+++ Recursion-Question/recursion-question.js
@@ -0,0 +1,5 @@
+// Alice and Bob are playing a game using a bunch of coins.The players pick several coins out of the bunch in turn. Each time a player is allowed to pick 1, 2, or 4 coins, and the player that gets the last coin is the winner. Given the number of coins and the names of the two players, you are required to write a program to calculate all the possible ways that the game can play out.
+
+const coinGame = () => {};
+
+console.log(coinGame(5, ['alice', 'bob']));
