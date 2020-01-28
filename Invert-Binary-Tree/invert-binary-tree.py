8986d3bdae1b6c49aebab7e2289c50cc11811e7d
edjunma <edjunma@gmail.com>
2020-01-28 02:54:00 -0500

    finish py solutions to invert-binary-tree

--- Invert-Binary-Tree/invert-binary-tree.py
+++ Invert-Binary-Tree/invert-binary-tree.py
@@ -4,5 +4,30 @@
 
 
 def invertBinaryTree(tree):
+    queue = [tree]
+    while len(queue):
+        current = queue.pop(0)
+        if current is None:
+            continue
+        swapLeftAndRight(current)
+        queue.append(current.left)
+        queue.append(current.right)
+
+
+def swapLeftAndRight(tree):
+    tree.left, tree.right = tree.right, tree.left
+
+# Solution #2
+# O(n) time | O(d) space
+
+
+def invertBinaryTree(tree):
     if tree is None:
         return
+    swapLeftAndRight(tree)
+    invertBinaryTree(tree.left)
+    invertBinaryTree(tree.right)
+
+
+def swapLeftAndRight(tree):
+    tree.left, tree.right = tree.right, tree.left
