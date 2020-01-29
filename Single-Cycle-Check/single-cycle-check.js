28b715c2a3698862d9da6f74e59d22ff3263fb1f
edjunma <edjunma@gmail.com>
2020-01-29 04:59:03 -0500

    start on single-cycle-check question

---
+++ Single-Cycle-Check/single-cycle-check.js
@@ -0,0 +1,7 @@
+// JavaScript Solution
+// O(n) time | O(1) space - where n is the length of the input array
+
+function hasSingleCycle(array) {
+	let numElementsVisited = 0;
+	let currentIdx = 0;
+}
