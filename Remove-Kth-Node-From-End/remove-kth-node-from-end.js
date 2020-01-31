51980d445cd0ba1bcef608dfbed25cbcd307f304
edjunma <edjunma@gmail.com>
2020-01-31 04:01:15 -0500

    start on remove kth node from end starting with js solution

---
+++ Remove-Kth-Node-From-End/remove-kth-node-from-end.js
@@ -0,0 +1,16 @@
+// JavaScript Solution
+
+class LinkedList {
+	constructor(value) {
+		this.value = value;
+		this.next = next;
+	}
+}
+
+// O(n) time | O(1) space
+
+function removeKthNodeFromEnd(head, k) {
+	let counter = 1;
+	let first = head;
+	let second = head;
+}
