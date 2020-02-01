330b4b0a2c7aae122dd0d4c4181efa1d73459545
edjunma <edjunma@gmail.com>
2020-02-01 01:25:23 -0500

    finish js solutions to powerset

--- Powerset/powerset.js
+++ Powerset/powerset.js
@@ -17,3 +17,17 @@ function powerset(array, idx = null) {
 	}
 	return subsets;
 }
+
+// O(n*2^n) time | O(n*2^n) space
+
+function powerset(array) {
+	const subsets = [[]];
+	for (const ele of array) {
+		const length = subsets.length;
+		for (let i = 0; i < length; i++) {
+			const currentSubset = subsets[i];
+			subsets.push(currentSubset.concat(ele));
+		}
+	}
+	return subsets;
+}
