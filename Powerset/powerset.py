5da07042035c66e2f0f80f7e921cb4ca871ef0dd
edjunma <edjunma@gmail.com>
2020-02-01 01:29:07 -0500

    finish power solutions

--- Powerset/powerset.py
+++ Powerset/powerset.py
@@ -2,6 +2,22 @@
 # O(n*2^n) time | O(n*2^n) space
 
 
+def powerset(array, idx=None):
+    if idx is None:
+        idx = len(array) - 1
+    if idx < 0:
+        return [[]]
+    ele = array[idx]
+    subsets = powerset(array, idx - 1)
+    for i in range(len(subsets)):
+        currentSubset = subsets[i]
+        subsets.append(currentSubset + [ele])
+    return subsets
+
+# Python Solution #2
+# O(n*2^n) time | O(n*2^n) space
+
+
 def powerset(array):
     subsets = [[]]
     for ele in array:
