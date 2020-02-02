98f3fce50c599f3955a1d60e6426cfcc385ecc7f
edjunma <edjunma@gmail.com>
2020-02-02 02:44:23 -0500

    finish group anagrams

--- Group-Anagrams/group-anagrams.py
+++ Group-Anagrams/group-anagrams.py
@@ -28,3 +28,15 @@ def groupAnagrams(words):
     result.append(currentAnagramGroup)
 
     return result
+
+    # O(w * n * log(n)) time | O(wn) space - where w is the number of words and n is the length of the longest word
+
+    def groupAnagrams(words):
+        anagrams = {}
+        for word in words:
+            sortedWord = "".join(sorted(word))
+            if sortedWord in anagrams:
+                anagrams[sortedWord].append(word)
+            else:
+                anagrams[sortedWord] = [word]
+        return list(anagrams.values())
