70f2716518bc3a92b890b704a959ae6d6a10c6ec
edjunma <edjunma@gmail.com>
2020-02-06 01:55:23 -0500

    added a practice anagram question

---
+++ JavaScript-Syntax-Questions/Anagram-Question/anagram.js
@@ -0,0 +1,19 @@
+// Given two strings, return true if they are anagrams of each other. Remember an anagram is a word, phrase, or name formed by rearranging the ltters of another such for example cinema can be formed to iceman.
+
+function anagram(str1, str2) {
+	// replace all whitespace in string, split into the arrays, sort and rejoin as strings
+	sorted1 = str1
+		.toLowerCase()
+		.replace(/\s+/g, '')
+		.split('')
+		.sort()
+		.join();
+	sorted2 = str2
+		.toLowerCase()
+		.replace(/\s+/g, '')
+		.split('')
+		.sort()
+		.join();
+
+	return sorted1 === sorted2;
+}
