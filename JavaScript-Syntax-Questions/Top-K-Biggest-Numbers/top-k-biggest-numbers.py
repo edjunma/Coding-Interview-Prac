8b7cf365e138d1ffdc6715b973315d0b8ca2dc3d
edjunma <edjunma@gmail.com>
2020-02-17 00:41:23 -0500

    finish top k biggest number in python solution

--- JavaScript-Syntax-Questions/Top-K-Biggest-Numbers/top-k-biggest-numbers.py
+++ JavaScript-Syntax-Questions/Top-K-Biggest-Numbers/top-k-biggest-numbers.py
@@ -20,10 +20,34 @@
 
 # 3. k- find max
 
-#This will assume the array is not sorted and only has integers and no duplicates
-#input: [2,1,4,7,3], k = 3
+# This will assume the array is not sorted and only has integers and no duplicates
+# input: [2,1,4,7,3], k = 3
 #output: [7,4,3]
 
+
+def fillInHeap(arr, k):
+    results = []
+    for i in range(0, k):
+        heapq.heappush(results, arr[i])
+    return results
+
+
 def findTopK(arr, k):
+    if arr is None or len(arr) == 0 or k > len(arr):
+        # chat with interviewer to see how to handle
+        return []
+
+    # fill in the heap with k elements
+    results = fillInHeap(arr, k)
+    # walk through heap and compare with top of heap
+    for i in range(k, len(arr)):
+        currentElement = arr[i]
+        topOfHeap = results[0]
+        if (topOfHeap < currentElement):
+          # if larger than top of heap, pop, and then insert into heap
+            heapq.heappop(results)
+            heapq.heappush(results, currentElement)
+    return results
 
 
+print findTopK([2, 1, 4, 7, 3], 3)
