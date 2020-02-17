e9c390949e9ddedf095be65eaebb9ca8193a8b5b
edjunma <edjunma@gmail.com>
2020-02-16 23:36:44 -0500

    starting on practice question, top k biggest number in array

---
+++ JavaScript-Syntax-Questions/Top-K-Biggest-Numbers/top-k-biggest-numbers.js
@@ -0,0 +1,12 @@
+// Given an array of numbers, find the top K biggest numbers for the array.
+
+// 1. Question
+// 2. Assumption
+// 3. Solution
+// 4. Improvement
+// QASI (Question, Assumption, Solution, Improvement)
+
+// Questions to ask
+// 1. Are there duplicates?
+// 2. Input?
+// 3. Sorted or not sorted?
