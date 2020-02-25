eeec8731bcbc6d75bbe4c1e1e22606e37f4bb13e
edjunma <edjunma@gmail.com>
2020-02-25 01:04:05 -0500

    finish python solutions to same-bsts

--- Hard Level/Same-BSTs/same-bsts.py
+++ Hard Level/Same-BSTs/same-bsts.py
@@ -34,3 +34,47 @@ def getBiggerOrEqual(array):
         if array[i] >= array[0]:
             biggerOrEqual.append(array[i])
     return biggerOrEqual
+
+
+# Python Solution #2
+# O(n^2) time | O(d) space where n is the number of nodes in each array, respectively, and d is the depth of the BST that they represent
+
+def sameBsts(arrayOne, arrayTwo):
+    return areSameBsts(arrayOne, arrayTwo, 0, 0, float("-inf"), float("inf"))
+
+
+def areSameBsts(arrayOne, arrayTwo, rootIdxOne, rootIdxTwo, minVal, maxVal):
+    if rootIdxOne == -1 or rootIdxTwo == -1:
+        return rootIdxOne == rootIdxTwo
+
+    if arrayOne[rootIdxOne] != arrayTwo[rootIdxTwo]:
+        return False
+
+    leftRootIdxOne = getIdxOfFirstSmaller(arrayOne, rootIdxOne, minVal)
+    leftRootIdxTwo = getIdxOfFirstSmaller(arrayTwo, rootIdxTwo, minVal)
+    rightRootIdxOne = getIdxOfFirstBiggerOrEqual(arrayOne, rootIdxOne, maxVal)
+    rightRootIdxTwo = getIdxOfFirstBiggerOrEqual(arrayTwo, rootIdxTwo, maxVal)
+
+    currentValue = arrayOne[rootIdxOne]
+    leftAreSame = areSameBsts(
+        arrayOne, arrayTwo, leftRootIdxOne, leftRootIdxTwo, minVal, currentValue)
+    rightAreSame = areSameBsts(
+        arrayOne, arrayTwo, rightRootIdxOne, rightRootIdxTwo, currentValue, maxVal)
+
+    return leftAreSame and rightAreSame
+
+
+def getIdxOfFirstSmaller(array, startingIdx, minVal):
+    # Find the index of the first smaller value after the starting index. Make sure that this value is greater than or equal to the minVal, which is the value of the previous parent node in the BST. If it isn't, then that value is located in the left subtree of the previous parent node.
+    for i in range(startingIdx + 1, len(array)):
+        if array[i] < array[startingIdx] and array[i] >= minVal:
+            return i
+    return -1
+
+
+def getIdxOfFirstBiggerOrEqual(array, startingIdx, maxVal):
+    # Find the index of the first bigger/equal value after the starting index. Make sure that this value is smaller than maxVal, which is the value of the previous parent node in the BST. If it isn't, then that value is located in the right subtree of the previous parent node.
+    for i in range(startingIdx + 1, len(array)):
+        if array[i] >= array[startingIdx] and array[i] < maxVal:
+            return i
+    return -1
