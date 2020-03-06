0e2ab55a0ffc0fb38215f7bd1e38396893c6991b
edjunma <edjunma@gmail.com>
2020-03-05 23:59:31 -0500

    woring on pytohn solution

--- Hard Level/Max-Sum-Increasing-Subsequence/max-sum-inc-subseq.py
+++ Hard Level/Max-Sum-Increasing-Subsequence/max-sum-inc-subseq.py
@@ -6,3 +6,13 @@ def maxSumIncreasingSubsequence(array):
     sequences = [None for x in array]
     sums = [num for num in array]
     maxSumIdx = 0
+    for i in range(len(array)):
+        currentNum = array[i]
+        for j in range(0, i):
+            otherNum = array[j]
+            if otherNum < currentNum and sums[j] + currentNum >= sums[i]:
+                sums[i] = sums[j] + currentNum
+                sequences[i] = j
+        if sums[i] >= sums[maxSumIdx]:
+            maxSumIdx = i
+return [sums[maxSumIdx], buildSequence(array, se)]
