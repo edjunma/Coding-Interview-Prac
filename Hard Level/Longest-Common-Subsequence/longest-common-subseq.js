83673afea6bae79c972c20453556c80b2d9b76b6
edjunma <edjunma@gmail.com>
2020-03-07 06:22:20 -0500

    finishing up solution #4 in js for longest common subseq

--- Hard Level/Longest-Common-Subsequence/longest-common-subseq.js
+++ Hard Level/Longest-Common-Subsequence/longest-common-subseq.js
@@ -93,13 +93,27 @@ function buildSequence(lcs) {
 	return sequence;
 }
 
-
 // JavaScript Solution #4
 // O(nm) time | O(nm) space
 
 function longestCommonSubsequence(str1, str2) {
-  const lengths = [];
-  for (let i = 0; i < str2.length + 1; i++) {
-    lengths.push(new Array(str1.length + 1).fill(0));
-  }
+	const lengths = [];
+	for (let i = 0; i < str2.length + 1; i++) {
+		lengths.push(new Array(str1.length + 1).fill(0));
+	}
+	for (let i = 1; i < str2.length + 1; i++) {
+		for (let j = 1; j < str1.length + 1; j++) {
+			if (str2[i - 1] === str1[j - 1]) {
+				lengths[i][j] = lengths[i - 1][j - 1] + 1;
+			} else {
+				lengths[i][j] = Math.max(lengths[i - 1][j], lengths[i][j - 1]);
+			}
+		}
+	}
+	return buildSequence(lengths, str1);
+}
+
+function buildSequence(lengths, string) {
+	const sequence = [];
+	let i = lengths.length - 1;
 }
