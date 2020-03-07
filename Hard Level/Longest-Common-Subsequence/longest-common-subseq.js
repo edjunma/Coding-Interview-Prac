99935290f8c8f440e738709fc4b225856f06594c
edjunma <edjunma@gmail.com>
2020-03-06 23:23:44 -0500

    finish solution #2 in js for longest common subseq

--- Hard Level/Longest-Common-Subsequence/longest-common-subseq.js
+++ Hard Level/Longest-Common-Subsequence/longest-common-subseq.js
@@ -18,3 +18,32 @@ function longestCommonSubsequence(str1, str2) {
 	}
 	return lcs[str2.length][str1.length];
 }
+
+// JavaScript Solution #2
+// O(nm*min(n, m)) time | O((min(n, m))^2) space
+
+function longestCommonSubsequence(str1, str2) {
+	const small = str1.length < str2.length ? str1 : str2;
+	const big = str1.length >= str2.length ? str1 : str2;
+	const evenLcs = new Array(small.length + 1).fill([]);
+	const oddLcs = new Array(small.length + 1).fill([]);
+	for (let i = 1; i < big.length; i++) {
+		let currentLcs, previousLcs;
+		if (i % 2 === 1) {
+			currentLcs = oddLcs;
+			previousLcs = evenLcs;
+		} else {
+			currentLcs = evenLcs;
+			previousLcs = oddLcs;
+		}
+		for (let j = 1; j < small.length + 1; j++) {
+			if (big[i - 1] === small[j - 1]) {
+				currentLcs[j] = previousLcs[j - 1].concat(big[i - 1]);
+			} else {
+				currentLcs[j] =
+					previousLcs[j].length > currentLcs[j - 1].length ? previousLcs[j] : currentLcs[j - 1];
+			}
+		}
+	}
+	return big.length % 2 === 0 ? evenLcs[small.length] : oddLcs[small.length];
+}
