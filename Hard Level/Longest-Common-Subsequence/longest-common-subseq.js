2f6a88518966b84dc10c2a1e996a29a5382ce1ea
edjunma <edjunma@gmail.com>
2020-03-06 23:31:52 -0500

    finish js solution #3 to longest common subseq

--- Hard Level/Longest-Common-Subsequence/longest-common-subseq.js
+++ Hard Level/Longest-Common-Subsequence/longest-common-subseq.js
@@ -47,3 +47,48 @@ function longestCommonSubsequence(str1, str2) {
 	}
 	return big.length % 2 === 0 ? evenLcs[small.length] : oddLcs[small.length];
 }
+
+// JavaScript Solution #3
+// O(nm) time | O(nm) space
+
+function longestCommonSubsequence(str1, str2) {
+	const lcs = [];
+	for (let i = 0; i < str2 / length + 1; i++) {
+		const row = [];
+		for (let j = 0; j < str1.length + 1; j++) {
+			const entry = new Array(4);
+			entry[1] = 0;
+			row.push(entry);
+		}
+		lcs.push(row);
+	}
+	for (let i = 1; i < str2.length + 1; i++) {
+		for (let j = 1; j < str1.length + 1; j++) {
+			if (str2[i - 1] === str1[j - 1]) {
+				lcs[i][j] = [str2[i - 1], lcs[i - 1][j - 1][1] + 1, i - 1, j - 1];
+			} else {
+				if (lcs[i - 1][j][1] > lcs[i][j - 1][1]) {
+					lcs[i][j] = [null, lcs[i - 1][j][1], i - 1, j];
+				} else {
+					lcs[i][j] = [null, lcs[i][j - 1][1], i, j - 1];
+				}
+			}
+		}
+	}
+	return buildSequence(lcs);
+}
+
+function buildSequence(lcs) {
+	const sequence = [];
+	let i = lcs.length - 1;
+	let j = lcs[0].length - 1;
+	while (i !== 0 && j !== 0) {
+		let currentEntry = lcs[i][j];
+		if (currentEntry[0]) {
+			sequence.unshift(currentEntry[0]);
+		}
+		i = currentEntry[2];
+		j = currentEntry[3];
+	}
+	return sequence;
+}
