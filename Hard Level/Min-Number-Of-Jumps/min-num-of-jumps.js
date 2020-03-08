61a3d9f578823828286046d2374f7afb1e973782
edjunma <edjunma@gmail.com>
2020-03-08 04:48:46 -0400

    finish optimal solution in js for min num of jumps

--- Hard Level/Min-Number-Of-Jumps/min-num-of-jumps.js
+++ Hard Level/Min-Number-Of-Jumps/min-num-of-jumps.js
@@ -13,3 +13,22 @@ function minNumberOfJumps(array) {
 	}
 	return jumps[jumps.length - 1];
 }
+
+// JavaScript Solution #2
+// O(n) time | O(1) space
+
+function minNumberOfJumps(array) {
+	if (array.length === 1) return 0;
+	let jumps = 0;
+	let maxReach = array[0];
+	let steps = array[0];
+	for (let i = 1; i < array.length - 1; i++) {
+		maxReach = Math.max(maxReach, i + array[i]);
+		steps--;
+		if (steps === 0) {
+			jumps++;
+			steps = maxReach - i;
+		}
+	}
+	return jumps + 1;
+}
