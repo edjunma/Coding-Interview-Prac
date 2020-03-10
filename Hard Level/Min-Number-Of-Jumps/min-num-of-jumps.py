35d54c1f2ca1a9cc55786b830dd7ca3665b9dab9
edjunma <edjunma@gmail.com>
2020-03-09 23:46:30 -0400

    finish python solution #2 to min num of jumps

--- Hard Level/Min-Number-Of-Jumps/min-num-of-jumps.py
+++ Hard Level/Min-Number-Of-Jumps/min-num-of-jumps.py
@@ -10,3 +10,22 @@ def minNumberOfJumps(array):
             if array[j] >= i - j:
                 jumps[i] = min(jumps[j] + 1, jumps[i])
     return jumps[-1]
+
+# Python Solution #2
+# O(n) time | O(1) space
+
+def minNumberOfJumps(array):
+  if len(array) == 1:
+    return 0
+  jumps = 0
+  maxReach = array[0]
+  steps = array[0]
+  for i in range(1, len(array) - 1):
+    maxReach = max(maxReach, i + array[i])
+    steps -= 1
+    if steps == 0:
+      jumps += 1
+      steps = maxReach - i
+  return jumps + 1
+
+  
