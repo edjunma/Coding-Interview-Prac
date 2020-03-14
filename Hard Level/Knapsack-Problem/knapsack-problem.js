da273002dc207cc00ecd0a703a1af4fb07faaf0e
edjunma <edjunma@gmail.com>
2020-03-13 23:50:48 -0400

    working on js solution for knapsack problem

--- Hard Level/Knapsack-Problem/knapsack-problem.js
+++ Hard Level/Knapsack-Problem/knapsack-problem.js
@@ -3,4 +3,12 @@
 
 function knapsackProblem(items, capacity) {
 	const knapsackValues = [];
+	for (let i = 0; i < items.length + 1; i++) {
+		const row = new Array(capacity + 1).fill(0);
+		knapsackValues.push(row);
+	}
+	for (let i = 1; i < items.length + 1; i++) {
+		const currentWeight = items[i - 1][1];
+		const currentValue = items[i - 1][0];
+	}
 }
