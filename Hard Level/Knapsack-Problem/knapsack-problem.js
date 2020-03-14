8a4824b5454569ee3b4c7f9d91269c8547456171
edjunma <edjunma@gmail.com>
2020-03-13 23:53:07 -0400

    update

--- Hard Level/Knapsack-Problem/knapsack-problem.js
+++ Hard Level/Knapsack-Problem/knapsack-problem.js
@@ -10,5 +10,16 @@ function knapsackProblem(items, capacity) {
 	for (let i = 1; i < items.length + 1; i++) {
 		const currentWeight = items[i - 1][1];
 		const currentValue = items[i - 1][0];
+		for (let c = 0; c < capacity + 1; c++) {
+			if (currentWeight > c) {
+				knapsackValues[i][c] = knapsackValues[i - 1][c];
+			} else {
+				knapsackValues[i][c] = Math.max(
+					knapsackValues[i - 1][c],
+					knapsackValues[i - 1][c - currentWeight] + currentValue
+				);
+			}
+		}
 	}
+	return [knapsackValues[items.length][capacity], getKnapsackItems(knapsackValues, items)];
 }
