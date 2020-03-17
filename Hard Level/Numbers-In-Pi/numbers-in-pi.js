c5e1fefd2da38bf24bd6d5394d41db3d03cdc8d5
edjunma <edjunma@gmail.com>
2020-03-17 19:26:58 -0400

    finish solution #2 to numbers in pi

--- Hard Level/Numbers-In-Pi/numbers-in-pi.js
+++ Hard Level/Numbers-In-Pi/numbers-in-pi.js
@@ -29,15 +29,28 @@ function getMinSpaces(pi, numbersTable, cache, idx) {
 // O(n^3 + m) time | O(n + m) space - where n is the number of digits in Pi and m is the number of favorite number
 
 function numbersInPi(pi, numbers) {
-  const numbersTable = {};
-  for (const number of numbers) {
-    numbersTable[number] = true;
-  }
-  const cache = {};
-  for (let i = pi.length - 1; i >= 0; i--) {
-    getMinSpaces(pi, numbersTable, cache, i);
-  }
-  return cache[0] === Infinity ? -1 : cache[0];
+	const numbersTable = {};
+	for (const number of numbers) {
+		numbersTable[number] = true;
+	}
+	const cache = {};
+	for (let i = pi.length - 1; i >= 0; i--) {
+		getMinSpaces(pi, numbersTable, cache, i);
+	}
+	return cache[0] === Infinity ? -1 : cache[0];
 }
 
-function getMinSpaces
+function getMinSpaces(pi, numbersTable, cache, idx) {
+	if (idx === pi.length) return -1;
+	if (idx in cache) return cache[idx];
+	let minSpaces = Infinity;
+	for (let i = idx; i < pi.length; i++) {
+		const prefix = pi.slice(idx, i + 1);
+		if (prefix in numbersTable) {
+			const minSpacesInSuffix = getMinSpaces(pi, numbersTable, cache, i + 1);
+			minSpaces = Math.min(minSpaces, minSpacesInSuffix + 1);
+		}
+	}
+	cache[idx] = minSpaces;
+	return cache[idx];
+}
