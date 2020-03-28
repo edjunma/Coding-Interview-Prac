02608cd0000b1b4a2ae28659deeee1a7948e3eeb
edjunma <edjunma@gmail.com>
2020-03-27 23:41:48 -0400

    finish topological sort #2 js question

--- Hard Level/Topological-Sort/topological-sort.js
+++ Hard Level/Topological-Sort/topological-sort.js
@@ -113,4 +113,29 @@ class JobGraph {
 			this.addNode(job);
 		}
 	}
+
+	addDep(job, dep) {
+		const jobNode = this.getNode(job);
+		const depNode = this.getNode(dep);
+		jobNode.deps.push(depNode);
+		depNode.numOfPrereqs++;
+	}
+
+	addNode(job) {
+		this.graph[job] = new JobNode(job);
+		this.nodes.push(this.graph[job]);
+	}
+
+	getNode(job) {
+		if (!(job in this.graph)) this.addNode(job);
+		return this.graph[job];
+	}
+}
+
+class JobNode {
+	constructor(job) {
+		this.job = job;
+		this.deps = [];
+		this.numOfPrereqs = 0;
+	}
 }
