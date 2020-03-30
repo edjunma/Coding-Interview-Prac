fcac516276f1153dc5bb1fb546b1dfc1eed95dbd
edjunma <edjunma@gmail.com>
2020-03-30 16:32:22 -0400

    finish python solution #1 to topological sort

--- Hard Level/Topological-Sort/topological-sort.py
+++ Hard Level/Topological-Sort/topological-sort.py
@@ -24,18 +24,48 @@ def getOrderedJobs(graph):
             return []
     return orderedJobs
 
+
 def depthFirstTraverse(node, orderedJobs):
-  if node.visited:
+    if node.visited:
+        return False
+    if node.visiting:
+        return True
+    node.visiting = True
+    for prereqNode in node.prereqs:
+        containsCycle = depthFirstTraverse(prereqNode, orderedJobs)
+        if containsCycle:
+            return True
+    node.visited = True
+    node.visiting = False
+    orderedJobs.append(node.job)
     return False
-  if node.visiting:
-    return True
-  node.visiting = True
-  for prereqNode in node.prereqs:
-    containsCycle = depthFirstTraverse(prereqNode, orderedJobs)
-    if containsCycle:
-      return True
-  node.visited = True
-  node.visiting = False
-  orderedJobs.append(node.job)
-  return False
-  
+
+
+class JobGraph:
+    def __init__(self, jobs):
+        self.nodes = []
+        self.graph = {}
+        for job in jobs:
+            self.addNode(job)
+
+    def addPrereq(self, job, prereq):
+        jobNode = self.getNode(job)
+        prereqNode = self.getNode(preeq)
+        jobNode.prereqs.append(prereqNode)
+
+    def addNode(self, job):
+        self.graph[job] JobNode(job)
+        self.nodes.append(self.graph[job])
+
+    def getNode(self, job):
+        if job not in self.graph:
+            self.addNode(job)
+        return self.graph[job]
+
+
+class JobNode:
+    def __init__(self, job):
+        self.job = job
+        self.prereqs = []
+        self.visited = False
+        self.visiting = False
