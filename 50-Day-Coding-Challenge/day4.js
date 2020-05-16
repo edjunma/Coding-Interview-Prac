1ddeafd8f73c0b557c093655631fa2d749789c68
edjunma <edjunma@gmail.com>
2020-05-16 02:36:34 -0400

    add day 4

---
+++ 50-Day-Coding-Challenge/day4.js
@@ -0,0 +1,13 @@
+// Day 4 JavaScript Coding Challenge
+// Print the multiplication tables with numbers from 1 to 10
+
+for (let i = 1; i <= 10; i++) {
+  return(i);
+}
+
+function printTable(n) {
+  for (let i = 1; i <= 10; i++) {
+    let row = n + " * " + i + " = " + n * i;
+    return row;
+  }
+}
--- day1.js
+++
@@ -1,6 +0,0 @@
-// Day 1 JavaScript Coding Challenge
-// Print numbers from 1 to 10
-
-for (let i = 1; i <= 10; i++) {
-	return i;
-}
