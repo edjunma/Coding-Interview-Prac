688dfa6f59bfcc7171f8add0176de2e414359cf3
edjunma <edjunma@gmail.com>
2020-05-17 06:27:01 -0400

    add day 17

---
+++ 50-Day-Coding-Challenge/day17.js
@@ -0,0 +1,16 @@
+// Day 15 JavaScript Coding Challenge
+// Calculate the sum of digits of a positive integer number
+
+function sumDigits(n) {
+	let s = n.toString();
+	let sum = 0;
+
+	for (let char of s) {
+		let digit = parseInt(char);
+		sum += digit;
+	}
+	return sum;
+}
+
+let sum = sumDigits(1235231);
+println('Sum: ', sum);
