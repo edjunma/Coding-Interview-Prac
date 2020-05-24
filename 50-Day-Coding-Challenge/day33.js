687a27e759362215766887ee457544bed55e9718
edjunma <edjunma@gmail.com>
2020-05-24 05:35:26 -0400

    finish day 33

---
+++ 50-Day-Coding-Challenge/day33.js
@@ -0,0 +1,14 @@
+// Day 32 Coding Challenge
+// Calculate the sums of numbers received in a comma delimited string
+
+function sumCSV(s) {
+	let ar = s.split(',');
+	let sum = 0;
+
+	for (let n of ar) {
+		sum += parseFloat(n);
+	}
+	return sum;
+}
+
+println(sumCSV('1.5, 2.3, 3.1, 4, 5.5'));
