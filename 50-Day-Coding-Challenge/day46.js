f9d422c1d64418325d0cd8f3a90c0c09f014f563
edjunma <edjunma@gmail.com>
2020-05-28 01:23:26 -0400

    start on day 46 part A

---
+++ 50-Day-Coding-Challenge/day46.js
@@ -0,0 +1,14 @@
+// Day 46 Coding Challenge
+// Challenge #46A
+// Find the maximum number in a jagged array of numbers or array of numbers *Requirements: Use recursion
+
+// Solution 1
+
+var ar = [2, 4, 10, [12, 4, [100, 99], 4], [3, 2, 99], 0];
+var max = findMax(ar);
+println('Max = ', max);
+
+// Use recursion to find the maximum numeric value in an array of arrays
+function findMax(ar) {
+	let max = Infinity;
+}
