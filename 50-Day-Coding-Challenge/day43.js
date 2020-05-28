fbe60b16e671455af6ffcd917a12adb3067f3db9
edjunma <edjunma@gmail.com>
2020-05-28 00:59:41 -0400

    add day 43

---
+++ 50-Day-Coding-Challenge/day43.js
@@ -0,0 +1,22 @@
+// Day 43 Coding Challenge
+// Create a function that will receive a bi-dimensional array as argument and a number and will extract as a unidimensional array the column specified by the number
+
+var ar = [
+	['John', 120],
+	['Jane', 115],
+	['Thomas', 123],
+	['Mel', 112],
+	['Charley', 122],
+];
+
+var numbers = extractCol(ar, 1);
+println(numbers);
+
+function extractCol(ar, colNo) {
+	var arCol = [];
+
+	for (let i = 0; i < ar.length; i++) {
+		arCol.push(ar[i][colNo]);
+	}
+	return arCol;
+}
